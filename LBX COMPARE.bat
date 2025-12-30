900279fa48b16230c51399292945c345431b6fe2
anaikmth <mthanaik@gmail.com>
2025-12-30 20:23:40 +0100

    Add files via upload

---
+++ LBX COMPARE.bat
@@ -0,0 +1,13 @@
+@echo off
+title Letterboxd Compare
+color 0A
+cls
+echo.
+echo  BY KAYOZZ
+echo.
+echo  [*] Demarrage du serveur...
+echo  [*] URL: http://localhost:3030
+echo.
+echo  [!] Appuyez sur Ctrl+C pour arreter
+echo.
+cargo run
