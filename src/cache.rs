4e2271629764559a1ab4b89c96c83419c15fe8df
anaikmth <mthanaik@gmail.com>
2026-01-08 19:04:01 +0100

    Update cache.rs
    Le cache enregistrait les profils et par conséquent les films vus (notes mises) n'étaient pas actualisés après la première entrée d'un profil. Corrigé.

--- src/cache.rs
+++ src/cache.rs
@@ -29,28 +29,18 @@ impl FileCache {
             Err(err) => return Err(err),
         };
 
-        // TODO: remove if file too old? if so possible race
-        // can use an rwlock in that case
-
         let mut contents = String::new();
         file.read_to_string(&mut contents)?;
         Ok(Some(contents))
     }
 
-    // TODO: make insert mutably borrow
+    // CORRECTION : Maintenant on écrase toujours le fichier
     pub fn insert(&self, key: &str, value: &str) -> Result<(), std::io::Error> {
-        // create_new is atomic, so possible race condition should be avoided
-        let mut file = match fs::OpenOptions::new()
+        let mut file = fs::OpenOptions::new()
             .write(true)
-            .create_new(true)
-            .open(Self::get_cache_file(key))
-        {
-            Ok(f) => f,
-            // TODO: what if the file already exists
-            // remove it and overwrite?
-            Err(e) if e.kind() == ErrorKind::AlreadyExists => return Ok(()),
-            Err(e) => return Err(e),
-        };
+            .create(true)      // ← Changé de create_new à create
+            .truncate(true)    // ← Ajouté pour vider le fichier avant d'écrire
+            .open(Self::get_cache_file(key))?;
 
         file.write_all(value.as_bytes())
     }
